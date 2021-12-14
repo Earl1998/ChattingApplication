d24f3e0334f0699cc72833b1039a54e67da34b11
Brad Earl <earlchristianlizada1998@gmail.com>
2021-12-14 10:22:32 +0800

    SRC - Disconnected

--- src/index.js
+++ src/index.js
@@ -36,4 +36,34 @@ io.on('connection', (socket) => {
         callback();
     });
 
-   
+    socket.on('sendMessage', (message, callback) => {
+        const user = getUser(socket.id);
+        const filter = new Filter();
+        if (filter.isProfane(message)) {
+            return callback('Profanity is not allowed')
+        }
+        io.to(user.room).emit('message', generateMessage(user.username, message));
+        callback();
+    });
+    socket.on('disconnect', () => {
+        const user = removeUser(socket.id);
+        if (user) {
+            io.to(user.room).emit('message', generateMessage('Admin', `${user.username} has left!`));
+            io.to(user.room).emit('roomData', {
+                room: user.room,
+                users: getUsersInRoom(user.room)
+            });
+        }
+    });
+
+    socket.on('sendLocation', (coords, callback) => {
+        const user = getUser(socket.id);
+        io.to(user.room).emit('locationMessage', generateLocationMessage(user.username, coords));
+        callback();
+    });
+});
+
+server.listen(port, () => {
+    console.log(`Server is up on port ${port}`);
+})
+    
