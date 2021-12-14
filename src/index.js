b879269c184f3c73363cb08d323448fd617e0faf
Brad Earl <earlchristianlizada1998@gmail.com>
2021-12-14 10:21:05 +0800

    SRC - Connection

--- src/index.js
+++ src/index.js
@@ -6,3 +6,34 @@ const Filter = require('bad-words');
 const {generateMessage, generateLocationMessage} = require('./utils/messages');
 const { addUser, removeUser, getUser, getUsersInRoom} = require('./utils/users');
 
+const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
+
+const port = process.env.PORT || 3000;
+const publicDirectoryPath = path.join(__dirname, '../public');
+
+app.use(express.static(publicDirectoryPath));
+
+io.on('connection', (socket) => {
+    console.log('New WebSocket connection');
+
+    socket.on('join', (options, callback) => {
+        const {error, user} = addUser({
+            id: socket.id,
+            ...options
+        });
+        if (error) {
+            return callback(error);
+        }
+        socket.join(user.room);
+        socket.emit('message', generateMessage('Admin', 'Welcome!'));
+        socket.broadcast.to(user.room).emit('message', generateMessage('Admin', `${user.username} has joined!`));
+        io.to(user.room).emit('roomData', {
+            room: user.room,
+            users: getUsersInRoom(user.room)
+        });
+        callback();
+    });
+
+   
