01e7db12a8cb74deb1d6a93ff11d4eb92224a293
Brad Earl <earlchristianlizada1998@gmail.com>
2021-12-14 10:15:11 +0800

    JS - Templetes

--- js/chat.js
+++ js/chat.js
@@ -7,3 +7,8 @@ const $messageFormButton = $messageForm.querySelector('button');
 const $sendLocationButton = document.querySelector('#send-location');
 const $messages = document.querySelector('#messages');
 
+
+const messageTemplate = document.querySelector('#message-template').innerHTML
+const locationMessageTemplate = document.querySelector('#location-message-template').innerHTML
+const sidebarTemplate = document.querySelector('#sidebar-template').innerHTML
+
